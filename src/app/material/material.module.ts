c03ca831f8a038ee2ced2fa48f322769579fc89b
Stefan <stefanfreising@gmx.de>
2018-09-28 23:01:46 +0200

    init

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import {Component} from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatButtonModule,
+  MatMenuModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatCardModule,
+  MatInputModule,
+  MatRadioModule,
+  MatSelectModule,
+  MatOptionModule
+} from '@angular/material';
+
+@NgModule({
+  imports: [
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatInputModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatOptionModule,
+    BrowserAnimationsModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatInputModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatOptionModule,
+    BrowserAnimationsModule
+  ]
+})
+export class MaterialModule {}
